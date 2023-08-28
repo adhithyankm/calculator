a23a96af08ba9943415d15ee0234d97e962b8c36
adhithyankm <adhikunnath007@gmail.com>
2023-08-28 20:46:01 +0530

    Calculator

---
+++ main.js
@@ -0,0 +1,25 @@
+// display
+
+function display(num){
+    output.value +=num;
+}
+
+// clear
+
+function clearAll(){
+    output.value=''
+}
+
+function evaluateExp(){
+
+
+
+
+    output.value=eval(output.value)
+
+}
+
+function removeLast(){
+    currentExp=output.value
+    output.value=currentExp.slice(0,-1)
+}
